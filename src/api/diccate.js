a542c7e2b08bc0d140c7113a3232ac4ee7d842cd
ruanteng <1983948356@qq.com>
2020-11-19 16:53:42 +0800

    11

--- src/api/diccate.js
+++ src/api/diccate.js
@@ -1,9 +1,33 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 export function getByUrl(params) {
   return request({
-    url: 'diccate/getbyurl',
-    method: 'get',
+    url: "diccate/getbyurl",
+    method: "get",
+    params:params
+  });
+}
+//  字典类型列表
+export function getAll(params) {
+  return request({
+    url: "diccate/getall",
+    method: "get",
+    params:params
+  });
+}
+// 添加字典类别
+export function addNew(params) {
+  return request({
+    url: "diccate/addnew",
+    method: "put",
     params
-  })
+  });
+}
+// 添加字典类别
+export function dicIdDelete(params) {
+  return request({
+    url: "diccate/delete",
+    method: "DELETE",
+    params: {params}
+  });
 }
