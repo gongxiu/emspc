e69992303db76866221784a0f6fa4075144145b3
rick <1983948356@qq.com>
2020-11-20 23:33:16 +0800

    11

--- src/views/maintain/maintainitem/index.vue
+++ src/views/maintain/maintainitem/index.vue
@@ -1,22 +1,13 @@
 <template>
-  <div>
-    <yearCalendar :showYearSelector="true" v-model="year" />
-  </div>
+    <div>我是设备保养项</div>
 </template>
 
 <script>
-import yearCalendar from "@/components/calendar/YearCalendar.vue";
-export default {
-  name: "index",
-  data() {
-    return {
-      year: "2020"
-    };
-  },
-  components: {
-    yearCalendar
-  }
-};
+    export default {
+        name: "index"
+    }
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
