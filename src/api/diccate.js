4948357137bc1f19c5c85e74041e0aeaf3c2ed2f
rick <1983948356@qq.com>
2020-11-26 22:39:54 +0800

    查询字典接口封装完成

--- src/api/diccate.js
+++ src/api/diccate.js
@@ -1,8 +1,20 @@
 import request from "@/utils/request";
-import qs from 'qs'
+import qs from "qs";
+
+const api = {
+  getbyurl: "diccate/getbyurl",
+  getAll: "diccate/getall",
+  addnew: "diccate/addnew",
+  delete: "diccate/delete",
+  update: "diccate/update",
+  deleteids: "diccate/deleteids",
+  updatecloumn: "diccate/updatecloumn",
+  getbyid: "diccate/getbyid"
+};
+// 根据url搜索字典类别
 export function getByUrl(params) {
   return request({
-    url: "diccate/getbyurl",
+    url: api.getbyurl,
     method: "get",
     params
   });
@@ -10,7 +22,7 @@ export function getByUrl(params) {
 //  字典类型列表
 export function getAll(params) {
   return request({
-    url: "diccate/getall",
+    url: api.getAll,
     method: "get",
     params
   });
@@ -18,24 +30,47 @@ export function getAll(params) {
 // 添加字典类别
 export function addNew(params) {
   return request({
-    url: "diccate/addnew",
+    url: api.addNew,
     method: "put",
     params
   });
 }
-// 添加字典类别
+// 删除字典类别
 export function dicIdDelete(params) {
   return request({
-    url: "diccate/delete",
+    url: api.delete,
     method: "DELETE",
-    params,
+    params
   });
 }
-
+// 更新字典类别
 export function dicUpdate(params) {
   return request({
-    url: "diccate/update",
+    url: api.update,
     method: "POST",
-    data:qs.parse(params)
+    data: qs.parse(params)
+  });
+}
+// 删除多条数据
+export function deleteids(params) {
+  return request({
+    url: api.deleteids,
+    method: "delete",
+    params
+  });
+}
+// 更新某些列
+export function updatecloumn(params) {
+  return request({
+    url: api.updatecloumn + params,
+    method: "post"
+  });
+}
+// 根据id和状态查询数据
+export function getbyid(params) {
+  return request({
+    url: api.getbyid,
+    method: "get",
+    params
   });
 }
