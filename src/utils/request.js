732c0b2069aff0d6cffe68ad1a33609ff5246284
chenyongfa <1980574155@qq.com>
2020-12-16 20:38:32 +0800

    更新

--- src/utils/request.js
+++ src/utils/request.js
@@ -17,7 +17,8 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['Authorization'] ='Bearer '+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW4iLCJVc2VySWQiOiI0OTQ5NGVkZC0wNTRhLTZlMDQtMmFhMy02MTVjOTAxNDk0MGIiLCJleHAiOjE2MDc1MjgzNDUsImlzcyI6IndlYmFwaS5jbiIsImF1ZCI6IldlYkFwaSJ9.vFcbd0npy-o64k6Li4h7S-Wm32aSLfD71ydS0gzaKlM'
+
+      config.headers['Authorization'] ='Bearer '+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW4iLCJVc2VySWQiOiI0OTQ5NGVkZC0wNTRhLTZlMDQtMmFhMy02MTVjOTAxNDk0MGIiLCJleHAiOjE2MDgxMzQzODEsImlzcyI6IndlYmFwaS5jbiIsImF1ZCI6IldlYkFwaSJ9.BPnSOhRco-Q24O6p_VjFiO_DszS0MAm0l75AsBhZdew"
         // 'Bearer ' + getToken()
     }
     return config
@@ -62,7 +63,7 @@ service.interceptors.response.use(
         //   location.reload()
         // })
       }
-      // return Promise.reject(new Error(res.message || 'Error'))
+      return Promise.reject(new Error(res.message || 'Error'))
     } else {
       return res
     }
